cd020c83a14fedfa7f66891301ea72927beebdf9
Ekaterina <145068362+keyu912@users.noreply.github.com>
2024-11-03 23:26:47 +0300

    Лабораторная работа 3

---
+++ Лабораторная работа 3/task_1.py
@@ -0,0 +1,13 @@
+def first_el(products, need_item):
+    for i in range(len(products)):
+        if products[i] == need_item:
+            return i
+    return None
+items_list = ['яблоко', 'банан', 'апельсин', 'груша', 'киви', 'банан']
+
+for find_item in ['банан', 'груша', 'персик']:
+    index_item = first_el(items_list, find_item)  # TODO Вызовите функцию, что получить индекс товара
+    if index_item is not None:
+        print(f"Первое вхождение товара '{find_item}' имеет индекс {index_item}.")
+    else:
+        print(f"Товар '{find_item}' не найден в списке.")
